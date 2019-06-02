131b92924700f78c8a6a399e51d21d0afb8bbf54
Carlos Frias SysAdmin <49353202+carlosfriascf@users.noreply.github.com>
2019-06-01 20:52:34 -0400

    Delete clivcp.sh

--- clivcp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget repo.sysdop.com/php.ini -O /etc/php.ini;/bin/cp -r /etc/php.ini /usr/local/vesta/php/lib/php.ini;/usr/local/vesta/php/bin/php /usr/local/vesta/softaculous/cli.php --repair
